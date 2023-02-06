e8fc4cc4648ec1f2f3a66fa64a17b31eafb9a719
nahidafzali <nahid.afzali1989@gmail.com>
2023-02-06 15:12:56 +0330

    Add code

---
+++ ParticleSwarmOptimization/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ParticleSwarmOptimization
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var lowerBound = new double[] { 0.0, 0.0 };
+            var upperBound = new double[] { 1.0, 1.0 };
+            var swarm = new ParticleSwarm(30, lowerBound, upperBound, p =>
+            {
+                return Math.Cos(p[0]) + Math.Sin(p[1]);
+            });
+
+            swarm.Step(50, i => {
+                Console.WriteLine($"{i}; {swarm.BestFitness}; {swarm.BestPosition[0]}; {swarm.BestPosition[1]}");
+                return Console.KeyAvailable;
+            });
+            Console.ReadKey();
+        }
+    }
+}
